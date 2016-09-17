66283830d58c9d606aa86a11bcae19cc671a2a2a
Brighton Trugman <Brighton Trugman>
2016-09-17 03:23:20 -0400

    New project

---
+++ src/StatisticsAnalyzer.java
@@ -0,0 +1,9 @@
+public class StatisticsAnalyzer {
+
+	public static void main(String[] args) {
+        WebsiteStream websiteStream = new WebsiteStream();
+		websiteStream.streamWebsite();
+		System.out.println("DODO");
+		
+    }
+}
