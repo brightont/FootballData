8a84758e9d8ef1b505e4ef19952d4e524f17285d
Brighton Trugman <Brighton Trugman>
2016-09-30 00:27:24 -0400

    Added Stat View controller

--- src/Controller/TeamChooserController.java
+++ src/Controller/TeamChooserController.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import MainApplication.MainApplication;
 import Model.Teams;
 import javafx.beans.property.ObjectProperty;
@@ -7,10 +10,16 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 public class TeamChooserController {
 
+	private static final Logger logger = Logger.getLogger("TeamChooserController");
+	
 	private MainApplication mainApplication;
 	
 	@FXML
@@ -19,7 +28,14 @@ public class TeamChooserController {
 	@FXML
 	private ComboBox<Teams> opponent;
 	
+	@FXML
+	private Button submit;
+	
 	private final ObjectProperty<Teams> teams = new SimpleObjectProperty<>();
+	
+	private String stringTeam;
+	
+	private String stringOpponent;
 	
 	public Teams getTeams() {
 		return teams.get();
@@ -27,6 +43,14 @@ public class TeamChooserController {
 	
 	public void setTeams(Teams team) {
 		teams.set(team);
+	}
+	
+	public String getStringTeam() {
+		return stringTeam;
+	}
+	
+	public String getStringOpponent() {
+		return stringOpponent; 
 	}
 	
 	private ObservableList<Teams> teamList = FXCollections.observableArrayList(Teams.values());
@@ -42,12 +66,25 @@ public class TeamChooserController {
 	
 	@FXML
 	public void pickTeam() {
+		stringTeam = teamOfChoice.getSelectionModel().getSelectedItem().toString();
+		stringOpponent = opponent.getSelectionModel().getSelectedItem().toString();
+	}
+
+	@FXML
+	public void pressSubmit() {
+		Stage stage;
+		Parent root;
 		
+		stage = (Stage) submit.getScene().getWindow();
+		try {
+			root = FXMLLoader.load(getClass().getResource("../view/StatView.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void setMainApp(MainApplication main) {
         mainApplication = main;
-        //teamOfChoice.setItems(teamList);
     }
 
 }
