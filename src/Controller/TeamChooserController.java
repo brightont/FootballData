9ecc624647b425b7a81c553c9239d8d822d1cfb2
Brighton Trugman <Brighton Trugman>
2016-09-29 21:03:31 -0400

    Main Screen Working

--- src/Controller/TeamChooserController.java
+++ src/Controller/TeamChooserController.java
@@ -1,22 +1,55 @@
 package Controller;
 
 import MainApplication.MainApplication;
+import Model.Teams;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class TeamChooserController {
 
+	private Stage dialogStage;
+	
 	private MainApplication mainApplication;
 	
 	@FXML
-	private ComboBox teamOfChoice;
+	private ComboBox<Teams> teamOfChoice;
 	
 	@FXML
 	private ComboBox opponent;
 	
-	@FXML
-    private void initialize() {
-      
+	private AnchorPane rootLayout;
+	
+	private final ObjectProperty<Teams> teams = new SimpleObjectProperty<>();
+	
+	public Teams getTeams(Teams team) {
+		return teams.get();
+	}
+	
+	public void setTeams(Teams team) {
+		teams.set(team);
+	}
+	
+	private final ObservableList<Teams> teamList = FXCollections.observableArrayList();
+	
+	
+	/*@FXML
+    public void initialize() {
+		teamOfChoice.setItems(teamList);
+    }
+	*/
+	public void setMainApp(MainApplication main) {
+
+        mainApplication = main;
+        //teamOfChoice.setItems(teamList);
+        //set the table view to contain the list of courses from the model
+
+        //automatically select the first table item and display the students
     }
 
 }
--- src/MainApplication/MainApplication.java
+++ src/MainApplication/MainApplication.java
@@ -16,30 +16,31 @@ import Controller.TeamChooserController;
 
 
 public class MainApplication extends Application{
-    private static final Logger logger = Logger.getLogger("MainApplication");
-
-    private Stage mainScreen;
-
-    private BorderPane rootLayout;
-
-	@Override
-	public void start(Stage primaryStage) {
-		 mainScreen = primaryStage;
-	     initRootLayout(mainScreen);
-	     showCourseOverview(mainScreen);
-	}
 	
-	public Stage getMainScreen() { 
-		 return mainScreen;
+	private static final Logger logger = Logger.getLogger("MainApplication");
+	
+	private Stage mainScreen;
+	
+	private AnchorPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		mainScreen = primaryStage;
+		initRootLayout(mainScreen);
+
+	}
+	 
+	public Stage getMainScreen() {
+		return mainScreen;
 	}
 	
 	private void initRootLayout(Stage mainScreen) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApplication.class.getResource("../view/FootballMainScreen.fxml"));
+            loader.setLocation(MainApplication.class.getResource("../view/FootballTeamChooser.fxml"));
             rootLayout = loader.load();
 
-            MainScreenController controller = loader.getController();
+            TeamChooserController controller = loader.getController();
             controller.setMainApp(this);
 
             mainScreen.setTitle("Football Data Analyzer");
@@ -50,30 +51,11 @@ public class MainApplication extends Application{
 
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Failed to find the fxml file for MainScreen!");
-            e.printStackTrace();
+            //e.printStackTrace();
         }
-    }
-	
-	private void showCourseOverview(Stage mainScreen) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApplication.class.getResource("../view/FootballTeamChooser.fxml"));
-            AnchorPane footballMainScreen = loader.load();
-
-            rootLayout.setCenter(footballMainScreen);
-
-            TeamChooserController controller = loader.getController();
-            //controller.setMainApp(this);
-
-        } catch (IOException e) {
-            //error on load, so log it
-            logger.log(Level.SEVERE, "Failed to find the fxml file for CourseOverview!!");
-            e.printStackTrace();
-        }
-
     }
 
 	public static void main(String[] args) {
-		launch(args);
+        launch(args);
     }
 }
